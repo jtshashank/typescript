42f60b68c9ac515bef3026561e5c1d0e402e21e1
Jain <Shashank_Jain4@Dellteam.com>
2019-01-09 10:58:28 +0530

    push new update

--- practice/function/function.js
+++ practice/function/function.js
@@ -1,45 +1,11 @@
-/*
-// function two example
-
-    function fun_one(arg1:any, arg2:any, arg3:any) : void {
-        document.write( arg1() + arg2() + arg3() )
-    }
-    function fun_two():string {
-        return "Angular 7";
-    }
-    function fun_three():string {
-        return "NodeJs";
-    }
-    function fun_four():string {
-        return " MongoDB" ;
-    }
-
-// Pass the arrgument under function of fun_one
-    fun_one(fun_two, fun_three, fun_four);
-
-*********** */
-// function third example
-/*
-    var my_array:Array<number> = [];
-
-    for(var i:number = 0; i <= 10; i++) {
-        my_array.push(i);
-        my_array.unshift(i);
-    }
-
-    for(var i:number = 0; i < my_array.length; i++) {
-          document.write(my_array[i]);
-    }
-*/
-// Function Fourth example
-var my_arrayy = [];
-function fun_onec() {
-    return " Welcome to name Function";
-}
-;
-for (var i = 0; i < 5; i++) {
-    my_arrayy.push(fun_onec);
-}
-for (var i = 0; i < my_arrayy.length; i++) {
-    document.write(my_arrayy[i]());
-}
+var dbfun = function (arg1, arg2) {
+    document.write(arg1() + arg2());
+};
+var SuccessCallback = function () {
+    return "Success....!";
+};
+var errorCallback = function () {
+    return "Error...!";
+};
+// document.write(SuccessCallback(), errorCallback());
+console.log(SuccessCallback(), errorCallback());
--- practice/function/function.ts
+++ practice/function/function.ts
@@ -1,54 +1,16 @@
 
-/*
-// function two example
 
-    function fun_one(arg1:any, arg2:any, arg3:any) : void {
-        document.write( arg1() + arg2() + arg3() )
-    }
-    function fun_two():string {
-        return "Angular 7";
-    }
-    function fun_three():string {
-        return "NodeJs";
-    }
-    function fun_four():string {
-        return " MongoDB" ;
-    }
+var dbfun = ( arg1:any, arg2:any): any => {
+	document.write(arg1() + arg2());
+}
 
-// Pass the arrgument under function of fun_one
-    fun_one(fun_two, fun_three, fun_four);
+var SuccessCallback = () : string => {
+	return "Success....!"
+}
 
-*********** */
+var errorCallback = () : string => {
+	return "Error...!"
+}
+document.write(SuccessCallback(), errorCallback())
 
-
-// function third example
-/*
-    var my_array:Array<number> = [];
-
-    for(var i:number = 0; i <= 10; i++) {
-        my_array.push(i);
-        my_array.unshift(i);
-    }
-
-    for(var i:number = 0; i < my_array.length; i++) {
-          document.write(my_array[i]);
-    }
-*/
-
-// Function Fourth example
-
-/*
-var my_arrayy:Array<any> = [];
-
-    function fun_onec():string {
-        return " Welcome to name Function";
-    };
-    for(var i:number = 0; i<5; i++) {
-        my_arrayy.push(fun_onec);
-    }
-    for(var i:number=0; i<my_arrayy.length; i++) {
-        document.write(my_arrayy[i]())
-    }
-
-*/
-
+console.log(SuccessCallback(), errorCallback());
